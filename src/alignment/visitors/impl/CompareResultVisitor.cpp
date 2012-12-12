4eb8055ec88c649990bda5b11cffd77e85de2877
edrezen <edrezen@irisa.fr>
2012-12-12 11:31:10 +0100

    First version (dedicated to gat)

---
+++ src/alignment/visitors/impl/CompareResultVisitor.cpp
@@ -0,0 +1,48 @@
+/*****************************************************************************
+ *                                                                           *
+ *   PLAST : Parallel Local Alignment Search Tool                            *
+ *   Version 2.0, released July  2011                                        *
+ *   Copyright (c) 2011                                                      *
+ *                                                                           *
+ *   PLAST is free software; you can redistribute it and/or modify it under  *
+ *   the CECILL version 2 License, that is compatible with the GNU General   *
+ *   Public License                                                          *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   CECILL version 2 License for more details.                              *
+ *****************************************************************************/
+
+#include <alignment/visitors/impl/CompareResultVisitor.hpp>
+#include <alignment/tools/impl/AlignmentOverlapCmd.hpp>
+
+#include <stdio.h>
+#define DEBUG(a)  //printf a
+
+using namespace std;
+
+using namespace database;
+
+using namespace alignment;
+using namespace alignment::core;
+using namespace alignment::tools::impl;
+
+/********************************************************************************/
+namespace alignment {
+namespace visitors  {
+namespace impl      {
+/********************************************************************************/
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+
+/********************************************************************************/
+}}} /* end of namespaces. */
+/********************************************************************************/
