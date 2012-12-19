1453050e48da873ab2b2beefa3c54cc256fec011
edrezen <edrezen@irisa.fr>
2012-12-19 18:51:43 +0100

    Bug 15202: set default line size for FASTA parsing to 2048 bytes

--- src/database/impl/FastaSequenceIterator.hpp
+++ src/database/impl/FastaSequenceIterator.hpp
@@ -72,7 +72,7 @@ public:
      */
     FastaSequenceIterator (
         const char* filename,
-        size_t commentMaxSize = 128*1024,
+        size_t commentMaxSize = 2*1024,
         u_int64_t offset0 = 0,
         u_int64_t offset1 = 0
     );
