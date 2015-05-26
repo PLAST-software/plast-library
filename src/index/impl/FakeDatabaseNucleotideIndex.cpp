0568ead47d14886c4d3e0a90e929bcdae34792bf
lantin <lantin@korilog.com>
2015-05-26 10:10:51 +0200

    A Fake indexator used to only create a bitmask for close homologs comparisons

---
+++ src/index/impl/FakeDatabaseNucleotideIndex.cpp
@@ -0,0 +1,159 @@
+/*****************************************************************************
+ *                                                                           *
+ *   PLAST : Parallel Local Alignment Search Tool                            *
+ *   Version 2.0, released May 2015                                          *
+ *   Copyright (c) 2015                                                      *
+ *                                                                           *
+ *   PLAST is free software; you can redistribute it and/or modify it under  *
+ *   the CECILL version 2 License, that is compatible with the GNU General   *
+ *   Public License                                                          *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   CECILL version 2 License for more details.                              *
+ *****************************************************************************/
+
+
+#include <index/impl/FakeDatabaseNucleotideIndex.hpp>
+
+#include <os/impl/DefaultOsFactory.hpp>
+
+#include <math.h>
+
+using namespace database;
+using namespace seed;
+using namespace indexation;
+
+/********************************************************************************/
+namespace indexation { namespace impl {
+/********************************************************************************/
+
+/********************************************************************************/
+inline static SeedHashCode revcomp (SeedHashCode x, size_t sizeKmer)
+{
+	u_int64_t res = x;
+
+	res = ((res>> 2 & 0x3333333333333333) | (res & 0x3333333333333333) <<  2);
+	res = ((res>> 4 & 0x0F0F0F0F0F0F0F0F) | (res & 0x0F0F0F0F0F0F0F0F) <<  4);
+	res = ((res>> 8 & 0x00FF00FF00FF00FF) | (res & 0x00FF00FF00FF00FF) <<  8);
+	res = ((res>>16 & 0x0000FFFF0000FFFF) | (res & 0x0000FFFF0000FFFF) << 16);
+	res = ((res>>32 & 0x00000000FFFFFFFF) | (res & 0x00000000FFFFFFFF) << 32);
+	res = res ^ 0xAAAAAAAAAAAAAAAA;
+
+	return (res >> (2*(32-sizeKmer))) ;
+}
+
+/********************************************************************************/
+
+
+enum { WORD_SIZE = sizeof(FakeDatabaseNucleotideIndex::word_t) * 8 };
+
+inline int bindex(int b)  { return b / WORD_SIZE; }
+inline int boffset(int b) { return b % WORD_SIZE; }
+
+#define SETMASK(data,b)  data[bindex(b)] |= 1LL << (boffset(b))
+
+
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+FakeDatabaseNucleotideIndex::FakeDatabaseNucleotideIndex (ISequenceDatabase* database, ISeedModel* model)
+    : AbstractDatabaseIndex (database, model), _maskOut(0), _maskSize(0)
+{
+	// compute the mask size
+	size_t alphabetSize = getModel()->getAlphabet()->size;
+	size_t maxSeedsNumber = pow((double)alphabetSize,(double)getModel()->getSpan());
+	_maskSize = maxSeedsNumber / WORD_SIZE;
+	_maskOut = new word_t [_maskSize];
+	// init the mask with 0 for all entries
+	memset (_maskOut, 0, sizeof(word_t)*_maskSize);
+}
+
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+FakeDatabaseNucleotideIndex::~FakeDatabaseNucleotideIndex ()
+{
+    delete[] _maskOut;
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+void FakeDatabaseNucleotideIndex::build ()
+{
+
+    for (size_t currentCode=0; currentCode<_maskSize; currentCode++)
+    {
+		/** We setup the mask */
+    	SETMASK (_maskOut, currentCode);
+		SETMASK (_maskOut, revcomp(currentCode, getModel()->getSpan()));
+//    	_maskOut[currentCode] = 1;
+    }
+
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+IDatabaseIndex::IndexEntry& FakeDatabaseNucleotideIndex::getEntry (const seed::ISeed* seed)
+{
+//	return NULL;
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+size_t FakeDatabaseNucleotideIndex::getOccurrenceNumber (const seed::ISeed* seed)
+{
+    return 0;
+}
+
+/*********************************************************************
+** METHOD  :
+** PURPOSE :
+** INPUT   :
+** OUTPUT  :
+** RETURN  :
+** REMARKS :
+*********************************************************************/
+u_int64_t FakeDatabaseNucleotideIndex::getTotalOccurrenceNumber ()
+{
+    return 0;
+}
+
+/********************************************************************************/
+} } /* end of namespaces. */
+/********************************************************************************/
+
+
+
+
---
+++ src/index/impl/FakeDatabaseNucleotideIndex.hpp
@@ -0,0 +1,95 @@
+/*****************************************************************************
+ *                                                                           *
+ *   PLAST : Parallel Local Alignment Search Tool                            *
+ *   Version 2.0, released May 2015                                          *
+ *   Copyright (c) 2015                                                      *
+ *                                                                           *
+ *   PLAST is free software; you can redistribute it and/or modify it under  *
+ *   the CECILL version 2 License, that is compatible with the GNU General   *
+ *   Public License                                                          *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
+ *   CECILL version 2 License for more details.                              *
+ *****************************************************************************/
+
+/** \file 	FakeDatabaseNucleotideIndex.hpp
+ *  \specific implementation of interfaces for close homologs nucleic database indexation.
+ *  \date 	05/05/2015
+ *  \author lantin
+ */
+
+#include <index/impl/AbstractDatabaseIndex.hpp>
+
+/********************************************************************************/
+namespace indexation {
+/** \specific implementation of interfaces for close homologs nucleic database indexation. */
+namespace impl {
+/********************************************************************************/
+
+/** \specific implementation of interfaces for close homologs nucleic database indexation.
+ *
+ * This implementation of the DatabaseIndex is a fake one. It will not really
+ * create an index but just create a seed mask to pass it to the two indexes which
+ * will index the query and the subject.
+ *
+ * The aim of this mask is to keep only some seeds to optimize a plastN comparison
+ * against a close homologs databank (like ARN-16s ones - silva or rdp for instance)
+ */
+class FakeDatabaseNucleotideIndex : public AbstractDatabaseIndex
+{
+public:
+
+	/** Constructor.
+	 * \param[in] database : the database to be indexed
+	 * \param[in] model : the seed model whose seeds are index keys
+	 * */
+	FakeDatabaseNucleotideIndex (database::ISequenceDatabase* database, seed::ISeedModel* model);
+	virtual ~FakeDatabaseNucleotideIndex ();
+
+	/** \copydoc AbstractDatabaseIndex::build */
+	void build ();
+
+	/** the only data created by this fake index */
+	u_int8_t* getMask ()  { return (u_int8_t*) _maskOut; }
+
+	/** */
+	typedef u_int64_t word_t;
+
+
+	/*****************************************************************/
+	/** 				NOT IMPLEMENTED METHODS 					**/
+	/*****************************************************************/
+	/** \copydoc AbstractDatabaseIndex::createOccurrenceIterator */
+	IOccurrenceIterator* createOccurrenceIterator (const seed::ISeed* seed, size_t neighbourhoodSize=0) { return 0; }
+
+	/** \copydoc AbstractDatabaseIndex::createOccurrenceBlockIterator */
+	IOccurrenceBlockIterator* createOccurrenceBlockIterator (
+	 const seed::ISeed* seed,
+	 size_t neighbourhoodSize,
+	 size_t blockSize
+	)  { return 0; }
+
+	/** \copydoc AbstractDatabaseIndex::getEntry */
+	IndexEntry& getEntry (const seed::ISeed* seed);
+
+	/** \copydoc AbstractDatabaseIndex::getOccurrenceNumber */
+	size_t getOccurrenceNumber (const seed::ISeed* seed);
+
+	/** \copydoc AbstractDatabaseIndex::getTotalOccurrenceNumber */
+	u_int64_t getTotalOccurrenceNumber ();
+
+	/** \copydoc AbstractDatabaseIndex::merge */
+	void merge (void) {}
+
+protected:
+
+	word_t* _maskOut;
+	size_t  _maskSize;
+
+}; // end of FakeDatabaseNucleotideIndex class
+
+/********************************************************************************/
+} } /* end of namespaces. */
+/********************************************************************************/
