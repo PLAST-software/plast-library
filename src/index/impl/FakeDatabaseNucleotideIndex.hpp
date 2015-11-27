21da0ba025dbbb520481f8f3a523308755d39903
Patrick Durand <patrick.durand@inria.fr>
2015-11-27 15:11:19 +0100

    update license to Affero GPL, update version number and copyright date

--- src/index/impl/FakeDatabaseNucleotideIndex.hpp
+++ src/index/impl/FakeDatabaseNucleotideIndex.hpp
@@ -5,13 +5,13 @@
  *   Copyright (c) 2015                                                      *
  *                                                                           *
  *   PLAST is free software; you can redistribute it and/or modify it under  *
- *   the CECILL version 2 License, that is compatible with the GNU General   *
+ *   the Affero GPL ver 3 License, that is compatible with the GNU General   *
  *   Public License                                                          *
  *                                                                           *
  *   This program is distributed in the hope that it will be useful,         *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
- *   CECILL version 2 License for more details.                              *
+ *   Affero GPL ver 3 License for more details.                              *
  *****************************************************************************/
 
 /** \file     FakeDatabaseNucleotideIndex.hpp
